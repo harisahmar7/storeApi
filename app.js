103f8901d94ad2306c49fb0e70a8347e77872fc2
harisahmar <harisahmar7@gmail.com>
2023-08-20 23:18:55 +0530

    Architech commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const morgran = require('morgan');
+const port = 4000;
+const dbConnection = require('./config/db_settings');
+const storeController = require('./controller/storecontroller');
+
+app.use(morgran('dev'));
+app.use(express.json());
+
+app.get('/products', storeController.getProduct);
+app.get('/product/static',storeController.getAllProductStatic);
+app.post('/saveProductData',storeController.saveProductAtOnce);
+
+const start = async()=>{
+	try{
+		await dbConnection();
+		app.listen(port, ()=>{
+		console.log(`Server is connected at port:${port}`);
+	})		
+	}catch(err){
+		console.log(err);
+	}
+}
+
+start();
+
